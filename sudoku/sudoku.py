80b0a58913a966c19a1e29c6af6a32971d12d49f
Brendan <brendanboissineau@gmail.com>
2021-06-16 12:48:18 -0400

    starting a sudoku program

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -2,7 +2,7 @@ from grid import CreateGrid
 import random
 
 if __name__ == '__main__':
-    x = CreateGrid(3, 3)
+    x = CreateGrid(9, 9)
     nums = list(range(0, (x.rows * x.cols), 1))
     random.shuffle(nums)
     idx = 0
