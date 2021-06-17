059bf33bb0e493a6b695bea120ab3f246fb1a8a8
boissineau <brendanboissineau@gmail.com>
2021-06-16 22:15:32 -0400

    added message functionality

--- message_project/message.py
+++ message_project/message.py
@@ -1,8 +1,8 @@
 import psycopg2
 
-def create_user(username):
-    exist = find_user(username)
-    if not exist:
+def create_user(con, username):
+    user = find_user(con, username)
+    if len(user) == 0:
         print('creating new user')
         with con:
             with con.cursor() as curs:
@@ -12,18 +12,32 @@ def create_user(username):
         print('User already exists')
 
 
-def find_user(username):
+def find_user(con, username):
     with con:
         with con.cursor() as curs:
             sql = """SELECT * from user_information WHERE username LIKE (%s)"""
             curs.execute(sql, (username,))
             rows = curs.fetchall()
-    if len(rows) > 0:
-        return True
-    else:
-        return False
+    return rows
 
+def create_message(con, username, message):
+    user_id = find_user(con, username)
+    user_id = user_id[0][0]
 
+    with con:
+        with con.cursor() as curs:
+            sql = """INSERT INTO messages (message, user_id) VALUES (%s, %s);"""
+            curs.execute(sql, (message, user_id))
+
+def find_messages(con, username):
+    user_id = find_user(con, username)
+    user_id = str(user_id[0][0])
+    with con:
+        with con.cursor() as curs:
+            sql = """SELECT message from messages WHERE user_id::text LIKE (%s)"""
+            curs.execute(sql, (user_id,))
+            messages = curs.fetchall()
+    return messages
 
 if __name__ == '__main__':
     con = psycopg2.connect(
@@ -36,9 +50,13 @@ if __name__ == '__main__':
     )
 
     username = 'lewis'
-    create_user(username)
-
-
+    message = 'ty!!!'
+    # create_message(con, username, message)
+    # create_user(con, username)
+    messages = find_messages(con, username)
+    if messages is not None:
+        for message in messages:
+            print(message[0])
 
     con.close()
 
